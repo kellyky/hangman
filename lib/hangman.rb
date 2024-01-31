795c2e38274c9fa8dcd4378d29dd8ac8ad4781db
Kelly Popko <kelpopko@gmail.com>
2024-01-30 18:49:18 -0600

    Add first full round of working Hangman

---
+++ lib/hangman.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+
+# A text-based game of hangman
+class Hangman
+  attr_reader :guesses_remaining
+
+  def self.play
+    wordlist = File.read('google-10000-english-no-swears.txt').split
+    new(wordlist).play
+  end
+
+  def initialize(wordlist)
+    @word = wordlist.select { |word| word.length >= 5 && word.length <= 12 }.shuffle.pop
+    @guesses_remaining = 5
+    @guessed_word = ''.rjust(@word.length, '_')
+  end
+
+  def play
+    evaluate_guess_count
+    player_turn
+    whole_word_guessed? ? announce_winner : play
+  end
+
+  def evaluate_guess_count
+    if @guesses_remaining == 5
+      welcome_player
+    elsif @guesses_remaining.zero?
+      game_over
+    end
+  end
+
+  def welcome_player
+    puts "\nWelcome to hangman!\n\n"
+    puts "Here's how to play:\n\n"
+    puts '  - The computer will choose a word. Your goal is to guess the word, one letter at a time.'
+    puts '  - If you do guess all the letters in time, you win!'
+    puts "  - If you guess incorrectly #{guesses_remaining} times, you lose.\n\n"
+    puts "The word has #{@word.length} letters.\n\n\n"
+  end
+
+  def player_turn
+    letter = guess_letter
+    if letter_in_word?(letter)
+      puts 'That letter is in the word!'
+      update_guessed_word(letter)
+    else
+      puts 'Not in the word.'
+      decrement_guesses
+      puts "You have #{@guesses_remaining} guesses left."
+    end
+
+    pretty_print_guessed_word
+    letter
+  end
+
+  def pretty_print_guessed_word
+    puts ""
+    puts @guessed_word.chars.join(' ')
+    puts ""
+  end
+
+  def guess_letter
+    print 'Pick a letter... '
+    letter = answer
+    puts "Ok, let's see if there are any '#{letter}'s..."
+    valid_guess?(letter) ? letter : try_again
+  end
+
+  def try_again
+    puts 'Ope, that\'s not an option. Try again.'
+    guess_letter
+  end
+
+  # TODO:
+  #   - add error handling
+  #   - validate that it is a letter
+  #   - validate that the letter has not yet been chosen
+  def valid_guess?(letter)
+    letter.downcase.match?(/[a-z]/)
+  end
+
+  def update_guessed_word(letter)
+    indices = []
+    @word.chars.each.with_index do |char, i|
+      indices << i if letter == char
+    end
+
+    indices.each do |index|
+      @guessed_word[index] = letter
+    end
+  end
+
+  def decrement_guesses
+    @guesses_remaining -= 1
+  end
+
+  def letter_in_word?(letter)
+    @word.include?(letter)
+  end
+
+  def whole_word_guessed?
+    # binding.pry
+    @guessed_word == @word
+  end
+
+  # TODO: DRY this up with game_over
+  def announce_winner
+    puts 'You guessed it - great job!'
+    play_again? ? Hangman.play : byebye
+  end
+
+  def game_over
+    puts "You ran out of turns. The word was '#{@word}'."
+    puts 'Better luck next time!'
+    play_again? ? Hangman.play : byebye
+  end
+
+  def play_again?
+    puts 'Would you like to play again?'
+    puts "Press '1' for yes and any other key to exit."
+    answer == '1'
+  end
+
+  def answer
+    gets.chomp
+  end
+
+  def byebye
+    puts "Ok, let's call it a day. Have a good one!"
+    exit
+  end
+end
+
+Hangman.play
---
+++ spec/hangman_spec.rb
@@ -0,0 +1,155 @@
+# frozen_string_literal: true
+
+require './lib/hangman'
+require 'pry-byebug'
+
+RSpec.describe Hangman do
+  subject { described_class.new(wordlist) }
+  let(:wordlist) { ['a', 'asdfasdfasdfasdf', 'banana'] }
+
+  describe '.play' do
+    it 'should call play' do
+      expect(subject).to receive(:play)
+      subject.play
+    end
+  end
+
+  describe '#play' do
+    it 'calls evaluate_guess_count' do
+    end
+
+    it 'calls player_turn' do
+    end
+
+    it 'checks if whole word is guessed' do
+    end
+
+    context 'when the word was guessed' do
+      # before { allow(subject).to receive(:whole_word_guessed).and_return(true) }
+      # it 'calls announce_winner' do
+      #   expect(subject).to receive(:announce_winner)
+      #   subject.play
+      # end
+    end
+
+    context 'when the word was not yet guessed' do
+      it 'calls play' do
+        expect(subject).to receive(:play)
+        subject.play
+      end
+    end
+  end
+
+  describe '#evaluate_guess_count' do
+    context 'when all guesses remain' do
+      it 'calls welcome sequence' do
+        subject.instance_variable_set(:@guesses_remaining, 5)
+        expect(subject).to receive(:welcome_player)
+        subject.evaluate_guess_count
+      end
+    end
+
+    context 'when no guesses remain' do
+      it 'calls game_over' do
+        subject.instance_variable_set(:@guesses_remaining, 0)
+        expect(subject).to receive(:game_over)
+        subject.evaluate_guess_count
+      end
+    end
+  end
+
+  describe '#welcome_player' do
+  end
+
+  describe '#player_turn' do
+    before { allow(subject).to receive(:answer).and_return('a') }
+    it 'something' do
+    end
+  end
+
+  describe '#guess_letter' do
+  end
+
+  describe '#try_again' do
+  end
+
+  describe '#valid_guess?' do
+  end
+
+  describe '#update_guessed_word' do
+  end
+
+  describe '#decrement_guesses' do
+    let!(:initial_guess_count) { subject.guesses_remaining }
+    it 'decrements remaining guesses by 1' do
+      subject.decrement_guesses
+      expect(subject.guesses_remaining).to eq(initial_guess_count - 1)
+    end
+  end
+
+  describe '#letter_in_word?' do
+    context 'when the letter does not appear in the word' do
+      it 'asdf' do
+        expect(subject.letter_in_word?('z')).to be(false)
+      end
+    end
+
+    context 'when the letter appears in the word' do
+      it 'asdf' do
+        expect(subject.letter_in_word?('a')).to be(true)
+      end
+    end
+  end
+
+  describe '#whole_word_guessed?' do
+  end
+
+  describe '#announce_winner' do
+  end
+
+  describe '#game_over' do
+    context 'when the player wants to play again' do
+      before { allow(subject).to receive(:play_again?).and_return(true) }
+
+      it 'should start a new game' do
+        expect(described_class).to receive(:play)
+        subject.game_over
+      end
+    end
+
+    context 'when the player does not want to play again' do
+      before { allow(subject).to receive(:play_again?).and_return(false) }
+
+      it 'should call byebye' do
+        expect(subject).to receive(:byebye)
+        subject.game_over
+      end
+    end
+  end
+
+  describe '#play_again' do
+    context 'when the player selects 1 to play again' do
+      before { allow(subject).to receive(:answer).and_return('1') }
+      it { expect(subject.play_again?).to be(true) }
+    end
+
+    context 'when the player selects something other than 1 to exit' do
+      before { allow(subject).to receive(:answer).and_return('x') }
+      it { expect(subject.play_again?).to be(false) }
+    end
+  end
+
+  # does this actually test answer though?
+  describe '#answer' do
+    it 'returns what the user entered' do
+      allow($stdin).to receive(:gets).and_return('yo')
+      answer = $stdin.gets
+
+      expect(answer).to eq('yo')
+    end
+  end
+
+  describe '#byebye' do
+    it { expect { subject.byebye }.to raise_exception SystemExit }
+  end
+end
